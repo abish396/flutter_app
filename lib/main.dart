8e71116e8c5ea622e6da9d8be0c5110206057285
Abhishek Sharma <abhishek.sharma0396@gmail.com>
2020-06-11 17:43:06 +0530

    dice app completed with randomization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -28,6 +30,15 @@ _DicePageState createState() => _DicePageState();
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,19 +47,17 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = 5;
-                });
+                changeDiceFace();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-                onPressed:  (){
-                  print('Right button pressed');
+                onPressed: () {
+                  changeDiceFace();
                 },
-                child: Image.asset('images/dice1.png')
+                child: Image.asset('images/dice$rightDiceNumber.png')
             ),
           ),
         ],
